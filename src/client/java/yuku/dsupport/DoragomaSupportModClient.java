2e536bf52939c6be5f90aa680b84758b701df39e
yukkuman <yutah822@gmail.com>
2025-05-11 20:31:36 +0900

    first

---
+++ src/client/java/yuku/dsupport/DoragomaSupportModClient.java
@@ -0,0 +1,10 @@
+package yuku.dsupport;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoragomaSupportModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
